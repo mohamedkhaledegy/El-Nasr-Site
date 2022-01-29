33b17ca67cf75a1653164d3e0e644481503fca11
Mohamed Khaled <mohamed_raya1@outlook.com>
2022-01-29 04:08:07 +0200

    mm

--- project/fix/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.template import context
-
-# Create your views here.
-
-
-def index(request):
-    context = {'name':'Mohamed'}
-    return render(request,'index.html',context)
